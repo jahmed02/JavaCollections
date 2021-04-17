a28c590e9df2c15c0eaf44b39a9a59745329cf1c
jahmed02 <jahmed22b@gmail.com>
2021-04-17 19:37:15 -0400

    collections

---
+++ src/dataStructure/UseHashMap.java
@@ -0,0 +1,30 @@
+package dataStructure;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class UseHashMap {
+    public static void main(String[] args) {
+
+        Map<String,String> map = new LinkedHashMap<String,String>();
+
+        map.put("USA","NY");
+        map.put("Canada","Toronto");
+        map.put("Australia","Sydney");
+
+        for (Map.Entry<String, String> iterate: map.entrySet()) {
+            System.out.println(iterate.getKey()+ "--->" + iterate.getValue());
+        }
+
+        Map<Integer, String> list = new LinkedHashMap<Integer, String>();
+
+        list.put(1, "Mango");
+        list.put(2, "Watermelon");
+        list.put (3, "papaya");
+
+        for (Map.Entry<Integer, String> itr: list.entrySet()) {
+            System.out.println(itr.getKey()+ "--->" + itr.getValue());
+        }
+
+    }
+}
